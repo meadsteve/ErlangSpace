a59d3ffdc071255660ebb743623e61d55a83e6d1
steve.brazier <steve.brazier@just-eat.com>
2014-11-12 08:39:55 +0000

    list tweak

--- src/shop.erl
+++ src/shop.erl
@@ -7,7 +7,9 @@ cost(apples)    -> 2;
 cost(pears)     -> 9;
 cost(milk)      -> 7;
 
-cost([{Item, Quantity} | RestOfList]) when Quantity > 0  -> (cost(Item) * Quantity) + cost(RestOfList);
-cost([ _SillyItem | RestOfList]) -> cost(RestOfList);
+cost({Item, Quantity}) when Quantity > 0  -> cost(Item) * Quantity;
+cost({_Item, _InvalidQuantity}) -> 0;
+
+cost([ListElement | RestOfList]) -> cost(ListElement) + cost(RestOfList);
 cost([]) -> 0.
 
